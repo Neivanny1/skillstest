8c3d60ab481f7d2a253692ff77a3fd56bba1be4b
Neivanny1 <nevileneivanny@gmail.com>
2024-05-26 15:40:09 +0000

    added about us page

--- challenge/views.py
+++ challenge/views.py
@@ -64,27 +64,6 @@ def admin_dashboard_view(request):
 
 
 '''
-Handles aboout information
-'''
-def aboutus_view(request):
-    return render(request,'about.html')
-
-'''
-Handles contact information
-'''
-def contactus_view(request):
-    sub = forms.ContactusForm()
-    if request.method == 'POST':
-        sub = forms.ContactusForm(request.POST)
-        if sub.is_valid():
-            email = sub.cleaned_data['Email']
-            name=sub.cleaned_data['Name']
-            message = sub.cleaned_data['Message']
-            send_mail(str(name)+' || '+str(email),message,settings.EMAIL_HOST_USER, settings.EMAIL_RECEIVING_USER, fail_silently = False)
-            return render(request, 'onsend.html')
-    return render(request, 'contact.html', {'form':sub})
-
-'''
 Displays all facilitators details
 BOth approved and pending
 '''
@@ -186,3 +165,26 @@ def delete_facilitator_view(request,pk):
     user.delete()
     facilitator.delete()
     return HttpResponseRedirect(reverse('viewfacilitator'))
+
+
+'''
+Handles aboout information
+'''
+def aboutus_view(request):
+    return render(request,'aboutus.html')
+
+
+'''
+Handles contact information
+'''
+def contactus_view(request):
+    sub = forms.ContactusForm()
+    if request.method == 'POST':
+        sub = forms.ContactusForm(request.POST)
+        if sub.is_valid():
+            email = sub.cleaned_data['Email']
+            name=sub.cleaned_data['Name']
+            message = sub.cleaned_data['Message']
+            send_mail(str(name)+' || '+str(email),message,settings.EMAIL_HOST_USER, settings.EMAIL_RECEIVING_USER, fail_silently = False)
+            return render(request, 'onsend.html')
+    return render(request, 'contact.html', {'form':sub})

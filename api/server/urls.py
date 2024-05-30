47df4c632748fa13d506e4f5ce81432e6ed1194f
Neivanny1 <nevileneivanny@gmail.com>
2024-05-30 16:06:13 +0300

    readme updted with picures

--- api/server/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import re_path
-
-from . import views
-
-urlpatterns = [
-    re_path('signup', views.signup),
-    re_path('login', views.login),
-    re_path('test_token', views.test_token),
-]

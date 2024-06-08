6471f04a361ff7fd3b7005bc05abb362afcb8e96
Neivanny1 <nevileneivanny@gmail.com>
2024-06-08 14:16:48 +0300

    sloveld pycopg2 error

--- skillstest/settings.py
+++ skillstest/settings.py
@@ -128,7 +128,7 @@ LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_L10N = True
-USE_TZ = True
+USE_TZ = False
 
 #Sending Emails Set up
 EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'

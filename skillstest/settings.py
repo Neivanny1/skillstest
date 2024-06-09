66a06fa885f476e76036d057d17eeb3e501d9d3b
Mwakazi_eric <123241068+Neivanny1@users.noreply.github.com>
2024-06-09 22:04:02 +0300

    Update settings.py

--- skillstest/settings.py
+++ skillstest/settings.py
@@ -32,7 +32,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1', 'localhost', 'mwakazieric.tech']
 
 # Application definition
 
